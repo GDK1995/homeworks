23bb9ab7a82c66f7e4bca15416a2a4f7b39fbdb9
GKD1995 <gulminez_95@mail.ru>
2022-04-17 14:37:49 +0600

    tasks array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++){
+      if (arr1[i] === arr2[i]) { result = true; }
+      else{ result = false; break; }
+    }
+  } else {
+    result = false;
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  filterArr = arr.filter(item => ( (item >= 0) && (item % 3 === 0) && (1/item !== Number.NEGATIVE_INFINITY) ) );
+  resultArr = filterArr.map(item => item * 10);
   return resultArr; // array
 }
